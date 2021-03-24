08fe766ab3b4d2b541765be83da1e2771c793c08
Baranga69 <keithbaranga40@gmail.com>
2021-03-24 21:17:23 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tulink/components/icon_bar.dart';
+import 'package:tulink/components/item_card.dart';
 
 void main() {
   runApp(MaterialApp());
@@ -9,7 +10,7 @@ class MaterialApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: NavBar(),
+      body: NavBar(), 
 
     );
   }
