e9574ae6c4980f8ee8a19f3edaab08e0b2710374
Baranga69 <keithbaranga40@gmail.com>
2021-03-24 10:58:10 +0300

    only the start
    Flick did this

---
+++ android/app/src/main/kotlin/com/example/tulink/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tulink
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/components/item_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ItemCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 0.0),
+      color: Colors.grey[500],
+      
+      
+    );
+  }
+}
